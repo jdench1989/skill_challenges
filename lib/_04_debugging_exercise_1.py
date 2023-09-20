274860a871d3f208434987a339b5e3b2b1b1aa88
jackdench <jdench1989@gmail.com>
2023-09-20 11:49:10 +0100

    Debugging exercise 1 complete

---
+++ lib/_04_debugging_exercise_1.py
@@ -0,0 +1,14 @@
+# Set a timer for 10 minutes.
+
+# Your challenge is to debug the program below before the timer goes off. 
+# When you fix the program or the timer goes off, move on to the next exercise.
+
+def say_hello(name):
+    return f"hello {name}"
+
+# original was missing the f at start of f"hello {name}"
+
+# Intended output:
+#
+# > say_hello("kay")
+# => "hello kay"
